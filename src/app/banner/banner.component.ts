c25529187a8028b2254e49eb178d7576a2e7e541
Kayden Miller <kaydenmiller1@gmail.com>
2024-01-12 18:28:41 -0700

    feat: add banner with kaysville ysa photos for wip

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {Gallery, GalleryComponent, GalleryItem, ImageItem} from "ng-gallery";
 
 @Component({
   selector: 'app-banner',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    GalleryComponent
   ],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
+  protected images: GalleryItem[] = [];
+
+  ngOnInit(): void {
+    this.images = [
+      new ImageItem({ src: './assets/kaysvilleysa_01.jpeg', thumb: './assets/kaysvilleysa_01.jpeg', alt: "Group Christmas Photo" }),
+      new ImageItem({ src: './assets/kay_02.jpeg', thumb: './assets/kay_02.jpeg', alt: "Christmas Photo" }),
+      new ImageItem({ src: './assets/kay_03.jpeg', thumb: './assets/kay_03.jpeg', alt: "Christmas Photo" }),
+    ]
+  }
 
 }
