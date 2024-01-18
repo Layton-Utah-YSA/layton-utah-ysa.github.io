5999469988de22ecd625dea9f993c42cdfadf9e9
Kayden Miller <kaydenmiller1@gmail.com>
2024-01-17 18:57:10 -0700

    feat: add some checks for hamburger menu

--- src/app/general-info/general-info.component.ts
+++ src/app/general-info/general-info.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {ActivityComponent} from "../activity/activity.component";
 import {ScheduleComponent} from "../schedule/schedule.component";
+import {LocationComponent} from "../location/location.component";
 
 @Component({
   selector: 'app-general-info',
   standalone: true,
   imports: [
     ActivityComponent,
-    ScheduleComponent
+    ScheduleComponent,
+    LocationComponent
   ],
   templateUrl: './general-info.component.html',
   styleUrl: './general-info.component.scss'
--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
 import {ChurchNamePlaqueComponent} from "../church-name-plaque/church-name-plaque.component";
+import {MatIconModule} from "@angular/material/icon";
+import {BreakpointObserver, BreakpointState} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-navigation-bar',
@@ -11,11 +13,32 @@ import {ChurchNamePlaqueComponent} from "../church-name-plaque/church-name-plaqu
     MatToolbarModule,
     MatButtonModule,
     RouterLink,
-    ChurchNamePlaqueComponent
+    ChurchNamePlaqueComponent,
+    MatIconModule
   ],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
-export class NavigationBarComponent {
+export class NavigationBarComponent implements OnInit {
+  protected shouldShowMenu: boolean = false;
+  protected isMenuOpen: boolean = false;
 
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe(['(min-width: 768px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.shouldShowMenu = false;
+        } else {
+          this.shouldShowMenu = true;
+        }
+      })
+  }
+
+  protected onMenuOpen() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
